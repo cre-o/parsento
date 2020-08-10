dcab44e73b354de2d4671ba2585fe0eeeb2ae0cd
creo <creo.ninja@gmail.com>
2020-08-10 12:35:39 +0300

    InvalidOption error

--- lib/parsento/error.rb
+++ lib/parsento/error.rb
@@ -3,4 +3,12 @@
 module Parsento
   class Error < StandardError; end
   class CLIOptionsError < Error; end
+  class InvalidOptionError < Error
+    attr_reader :option
+
+    def initialize(option)
+      super
+      @option = option
+    end
+  end
 end
