b0071d32064ea0ddcd6217e9b8b330733414ba95
creo <creo.ninja@gmail.com>
2020-08-10 12:12:32 +0300

    Help messages now can be shown
    better user experience!

--- spec/bin/integration_spec.rb
+++ spec/bin/integration_spec.rb
@@ -6,6 +6,20 @@ require 'helper_methods'
 describe 'Application integration testing' do
   before(:each) { @cmd_out = nil }
 
+  context 'Shows help message' do
+    before(:each) { @cmd_out = nil }
+
+    it 'With "--h" option' do
+      Open3.popen2('./bin/parsento -h') { |_i, o, _t| @cmd_out = o.gets(nil) }
+      expect(@cmd_out).to include('Usage: ./bin/parsento [options] [file_path]*')
+    end
+
+    it 'With "--help" option' do
+      Open3.popen2('./bin/parsento --help') { |_i, o, _t| @cmd_out = o.gets(nil) }
+      expect(@cmd_out).to include('Usage: ./bin/parsento [options] [file_path]*')
+    end
+  end
+
   context 'Shows human readable error messages' do
     it 'No command line options defined!' do
       Open3.popen2("./bin/parsento") { |_i, o, _t| @cmd_out = o.gets(nil) }
