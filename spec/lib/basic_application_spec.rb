bc6923f914b67f988ee93a19b7702cff8033c5d2
creo <creo.ninja@gmail.com>
2020-08-10 13:21:08 +0300

    BasicApplication spec with different init options

---
+++ spec/lib/basic_application_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rspec_helper'
+
+describe Parsento::BasicApplication do
+  let(:valid_file_path) { source_fixture('example.log') }
+  let(:reader) { Parsento::Reader.new(valid_file_path, :log) }
+
+  subject { Parsento::BasicApplication.new(reader) }
+
+  it { is_expected.to respond_to(:start) }
+
+  it '.start' do
+    expect(described_class.respond_to?(:start)).to be_truthy
+  end
+end
