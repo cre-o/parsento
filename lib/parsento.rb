c3941b9a673b398b63161217a2dd9454d9a41ce2
creo <creo.ninja@gmail.com>
2020-08-10 12:07:39 +0300

    Let's add some errors processing

--- lib/parsento.rb
+++ lib/parsento.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'parsento/error'
 require_relative 'parsento/basic_application'
 require_relative 'parsento/cli'
 require_relative 'parsento/version'
---
+++ lib/parsento/error.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Parsento
+  class Error < StandardError; end
+  class CLIOptionsError < Error; end
+end
