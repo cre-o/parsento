cc60c2caf21388c00e6fe665422255ef28ca6b07
creo <creo.ninja@gmail.com>
2020-08-10 18:09:41 +0300

    Emotions priority has been changed

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module Parsento
     def initialize(reader)
       super
       @reader_options = {
-        production: {max_lines: 1000, lists_separator: '¯\_(ツ)_/¯'},
-        development: {max_lines: 3000, lists_separator: '(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧'},
+        production: {max_lines: 1000, lists_separator: '(ﾉ◕ヮ◕)ﾉ*:・ﾟ✧'},
+        development: {max_lines: 3000, lists_separator: '¯\_(ツ)_/¯'},
         test: {max_lines: 1000, lists_separator: '•ᴗ•'}
       }
     end
