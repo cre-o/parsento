ea679586e67053a0e8d4bb4514a3933206727722
creo <creo.ninja@gmail.com>
2020-08-10 10:43:12 +0300

    Program version should be here

---
+++ lib/parsento/version.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module Parsento
+  # This module contains current version of library itself
+  module Version
+    STRING = '0.1.0'
+
+    def self.version
+      STRING
+    end
+  end
+end
