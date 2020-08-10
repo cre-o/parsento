8e9d9e338ccfcc02eb6ec31ba018346424bb8745
creo <creo.ninja@gmail.com>
2020-08-10 11:44:06 +0300

    No args error displayed

--- lib/parsento/cli.rb
+++ lib/parsento/cli.rb
@@ -5,11 +5,18 @@ module Parsento
   class CLI
     class << self
       def parse_options(args = ARGV)
-
+        print_and_exit 'No command line options defined!' if args.empty?
       end
 
       def run(opts)
 
+      end
+
+      private
+
+      def print_and_exit(text)
+        $stdout.puts(text)
+        Kernel.exit
       end
     end
   end
