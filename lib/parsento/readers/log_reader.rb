7e9e0d9a4fce3b52ac3e8be6204702ef0f99e6bb
creo <creo.ninja@gmail.com>
2020-08-10 18:08:01 +0300

    Final Log reader implementation

---
+++ lib/parsento/readers/log_reader.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module Parsento
+  # Extends knowledge about available readers
+  module Readers
+    # Reads .log files
+    module Log
+      def parse(file_path, options)
+        regexp = /^\/.*\s[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*$/
+        raw_array = IO.foreach(file_path).lazy.grep(regexp).take(options[:max_lines]).to_a
+        pages_h = store_in_hash(raw_array)
+
+        print_stats(pages_h, options)
+      end
+
+    private
+
+      def print_stats(pages_h, options)
+        sort_by_visits(pages_h).each { |a| print "#{a[0]} #{a[1][:visits]} visits\n" }
+        print "\n"
+        print "\e[1m#{options[:lists_separator]}\e[0m"
+        print "\n\n"
+        sort_by_unique_views(pages_h).each { |a| print "#{a[0]} #{a[1][:unique_ips_count]} unique views\n" }
+        print "\n"
+      end
+
+      def store_in_hash(raw_array)
+        pages_h = {}
+        raw_array.each do |v|
+          spltd = v.split
+          page  = spltd.first
+          ip    = spltd.last
+
+          if pages_h.key?(page)
+            pages_h[page][:visits] += 1
+            unless pages_h[page][:unique_ips].include?(ip)
+              pages_h[page][:unique_ips].push(ip)
+              pages_h[page][:unique_ips_count] += 1
+            end
+          else # db hash not exists for this page, create it!
+            pages_h[page] = {visits: 1, unique_ips: [ip], unique_ips_count: 1}
+          end
+        end
+
+        pages_h
+      end
+
+      def sort_by_visits(pages_h)
+        pages_h.sort { |s1, s2| s2[1][:visits] <=> s1[1][:visits] }
+      end
+
+      def sort_by_unique_views(pages_h)
+        pages_h.sort { |s1, s2| s2[1][:unique_ips_count] <=> s1[1][:unique_ips_count] }
+      end
+    end
+  end
+end
