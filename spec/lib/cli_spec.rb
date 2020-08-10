adbedfa113f98617a40363a127346f78a4d3a17d
creo <creo.ninja@gmail.com>
2020-08-10 11:22:32 +0300

    Basic CLI class methods

--- spec/lib/cli_spec.rb
+++ spec/lib/cli_spec.rb
@@ -3,5 +3,11 @@
 require 'rspec_helper'
 
 describe Parsento::CLI do
+  it '.parse_options' do
+    expect(described_class.respond_to?(:parse_options)).to be_truthy
+  end
 
+  it '.run' do
+    expect(described_class.respond_to?(:run)).to be_truthy
+  end
 end
