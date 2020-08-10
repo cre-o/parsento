9aca0d05c4928b849547defda6feeddb295e075f
creo <creo.ninja@gmail.com>
2020-08-10 13:22:40 +0300

    BasicApplication arrived for platform to be inherited from

--- lib/parsento/basic_application.rb
+++ lib/parsento/basic_application.rb
@@ -3,5 +3,20 @@
 module Parsento
   # Basic application logic (with possibility to extend)
   class BasicApplication
+    def initialize(reader)
+      # @TODO make validation
+      @reader = reader
+    end
+
+    # @TODO pass additional options to the parser
+    def start
+      @reader.parse
+    end
+
+    class << self
+      def start(file_to_read, file_type = :log)
+        new(Parsento::Reader.new(file_to_read, file_type)).start
+      end
+    end
   end
 end
