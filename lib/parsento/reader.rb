64618c168d18897a145f59845125e899c031d89f
creo <creo.ninja@gmail.com>
2020-08-10 14:44:32 +0300

    Basic Reader class

---
+++ lib/parsento/reader.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require_relative 'readers/log_reader'
+
+module Parsento
+  # Reading logic for the input file
+  class Reader
+    module Log
+      extend Parsento::Readers::Log
+    end
+    # @TODO create XML reader
+
+    def initialize(file_path, options)
+      @file_path = file_path
+      @options   = options
+
+      run_validations
+    end
+
+    # By default (and for simplicity) only one parser engine is configured
+    # Other engines can be connected at the top of current class and called here by file extension
+    def parse
+      if file_ext == '.log'
+        Log.parse(@file_path)
+      else
+        raise Parsento::ValidationError, "Correct reader engine for selected #{file_ext} file was not found."
+      end
+    end
+
+  private
+
+    def run_validations
+      validate_file_path && validate_options
+    end
+
+    def validate_file_path
+      raise Parsento::ValidationError, 'Invalid file path' unless File.file?(@file_path)
+    end
+
+    # No options in this version
+    def validate_options; end
+
+    def file_ext
+      File.extname(@file_path)
+    end
+  end
+end
