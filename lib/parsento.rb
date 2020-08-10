bec42dfc40b9bfab59e2ef843c096f89fab1e159
creo <creo.ninja@gmail.com>
2020-08-10 14:47:07 +0300

    Reader class was added to lib

--- lib/parsento.rb
+++ lib/parsento.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'parsento/error'
+require_relative 'parsento/reader'
 require_relative 'parsento/basic_application'
 require_relative 'parsento/cli'
 require_relative 'parsento/version'
