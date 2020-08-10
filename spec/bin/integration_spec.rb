e7247660b1c29362b9db499b2247420f941690e9
creo <creo.ninja@gmail.com>
2020-08-10 11:43:20 +0300

    It can display human readable errors (no command like options)

--- spec/bin/integration_spec.rb
+++ spec/bin/integration_spec.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
+require 'open3'
 require 'helper_methods'
 
 describe 'Application integration testing' do
+  before(:each) { @cmd_out = nil }
 
+  context 'Shows human readable error messages' do
+    it 'No command line options defined!' do
+      Open3.popen2("./bin/parsento") { |_i, o, _t| @cmd_out = o.gets(nil) }
+      expect(@cmd_out).to include('No command line options defined!')
+    end
+  end
 end
