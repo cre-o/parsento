03ba54e01714ca9f4345a658cd401b2484e75e38
creo <creo.ninja@gmail.com>
2020-08-14 11:18:46 +0300

    Comment ~> right combination of words

--- lib/parsento/readers/strategies/log_strategy.rb
+++ lib/parsento/readers/strategies/log_strategy.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Parsento
-  # Extends knowledge about available readers
+  # Extends knowledge about available parsing strategies
   module LogStrategy
     def self.sort_by_page_views(reader)
       calculator = Calculate.new(reader.file_path)
